77bdbfaaa45af85afc68f42edac55d5cd6fadd97
ShengHua Wu <fantasy0404@gmail.com>
2017-10-21 18:26:35 +0800

    Setup PostgreSQLProvider dependceny and build the project.

--- Sources/App/Setup/Config+Setup.swift
+++ Sources/App/Setup/Config+Setup.swift
@@ -1,4 +1,4 @@
-import FluentProvider
+import PostgreSQLProvider
 
 extension Config {
     public func setup() throws {
@@ -9,12 +9,13 @@ extension Config {
         try setupProviders()
         try setupPreparations()
     }
-    
+
     /// Configure providers
     private func setupProviders() throws {
         try addProvider(FluentProvider.Provider.self)
+        try addProvider(PostgreSQLProvider.Provider.self)
     }
-    
+
     /// Add all models that should have their
     /// schemas prepared before the app boots
     private func setupPreparations() throws {
